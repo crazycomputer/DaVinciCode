4ec9785fac5cfb5a615f8ee563922f339e12720a
crazycomputer <34932896+crazycomputer@users.noreply.github.com>
2019-05-19 01:24:34 +0800

    【项目结构调整】

--- server/src/test/java/code/starter/TestMainVerticle.java
+++ server/src/test/java/code/starter/TestMainVerticle.java
@@ -1,13 +1,7 @@
-package com.davinci.code.starter;
+package code.starter;
 
-import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
