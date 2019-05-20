6591accc4777839a95826636f698857780f26ece
zhanganrong <anrongzh@126.com>
2019-05-20 16:31:38 +0800

    identify the name&password

--- server/src/main/java/com/davinci/code/user/model/User.java
+++ server/src/main/java/com/davinci/code/user/model/User.java
@@ -3,10 +3,42 @@ package com.davinci.code.user.model;
 
 import lombok.Value;
 
-@Value
+
 public class User {
   private String name;
   private String password;
   private String userId;
   private String sex;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getSex() {
+    return sex;
+  }
+
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
 }
